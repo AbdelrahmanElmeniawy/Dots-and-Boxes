e62567c97c430690b61d2ce1497f32613e01b3c0
YousefHassan2000 <58993094+YousefHassan2000@users.noreply.github.com>
2019-12-18 00:19:54 +0200

    add AI_function

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 void game_function(int n, int mode, int size, char game[size][size],int totallines, int movesplayed[totallines][7], int noofmoves, int player,int score1,int score2, int moves1, int moves2);
 int checkbox(int row,int col,int size,char game[size][size],char compgame[size][size],int player,int totallines, int noofmoves, int movesplayed[totallines][7], int ai);
+void AI_function(int *row,int *col,int size,char game[size][size],char compgame[size][size],int player,int totallines, int noofmoves, int movesplayed[totallines][7], int v, int h, int dot);
 void printgame(int sizea, char game[sizea][sizea], int score1, int score2, int totallines, int noofmoves, int noOfMoves1, int noOfMoves2, int time0);
 
 int main()
@@ -99,69 +100,7 @@ void game_function(int n, int mode, int size, char game[size][size],int totallin
         printf("\tplayer %d's turn\n\n",player);
         setTextColor(stdout,TC_LIGHTGRAY);
         if(player==2 && mode == 1){
-            int s=0, AI = 1, i1,j1,u=0,notuserwin;
-            ////////
-            for(i=1;i<size;i++){
-                for(j=1;j<size;j++){
-                    if(game[i][j]==' '&&(compgame[i][j]== v ||compgame[i][j]==h)){
-                        game[i][j]=compgame[i][j];
-                        check=checkbox(i,j,size,game,compgame,player,totallines, noofmoves, movesplayed, AI);
-                        if(check==1||check==2){
-                            s=1;
-                            break;
-                        }
-                        else{
-                            game[i][j]=' ';
-                        }
-                    }
-                }
-                if(s)
-                    break;
-            }
-            if(s==0){
-                for(i=1; i<size;i++){
-                    for(j=1; j<size ; j++){
-                        if(game[i][j]==' '&&(compgame[i][j]== v ||compgame[i][j]==h)){
-                            game[i][j]=compgame[i][j];
-                            notuserwin=1;u=0;
-                            for(i1=1;i1<size;i1++){
-                                for(j1=1;j1<size;j1++){
-                                    if(game[i1][j1]==' '&&(compgame[i1][j1]== v ||compgame[i1][j1]==h)){
-                                        game[i1][j1]=compgame[i1][j1];
-                                        check=checkbox(i1,j1,size,game,compgame,player,totallines, noofmoves, movesplayed, AI);
-                                        if(check==1||check==2){
-                                            game[i][j]=' ';
-                                            game[i1][j1]=' ';
-                                            u=1;
-                                            notuserwin=0;
-                                            break;
-                                        }
-                                        else game[i1][j1]=' ';
-                                    }
-                                }
-                                if(u) break;
-                            }
-                            if(notuserwin){s=1; break;}
-                        }
-                    }
-                    if(s) break;
-                }
-            }
-            if(s == 0){
-                for(i = 1; i < size; i++){
-                    int bug = 0;
-                    for(j = 1; j < size; j++){
-                        if(game[i][j] == ' ' && (compgame[i][j] == v || compgame[i][j]== h)){
-                            bug = 1;
-                            break;
-                        }
-                    }
-                    if (bug){
-                        break;
-                    }
-                }
-            }
-            row = i; col = j;
+            AI_function(&row , &col,size,game,compgame,player,totallines, noofmoves, movesplayed, v, h, dot);
         }
         else{
             if(player==1){
@@ -316,6 +255,69 @@ void game_function(int n, int mode, int size, char game[size][size],int totallin
             }
         }
     }
+}
+
+void AI_function(int *row,int *col,int size,char game[size][size],char compgame[size][size],int player,int totallines, int noofmoves, int movesplayed[totallines][7], int v, int h, int dot)
+{
+    int s=0, i1,j1,u=0,notuserwin, AI = 1, i, j, check;
+    for(i=1;i<size;i++){
+        for(j=1;j<size;j++){
+            if(game[i][j]==' '&&(compgame[i][j]== v ||compgame[i][j]==h)){
+                game[i][j]=compgame[i][j];
+                check=checkbox(i,j,size,game,compgame,player,totallines, noofmoves, movesplayed, AI);
+                if(check==1||check==2){
+                    s=1;
+                    break;
+                }
+                else{
+                    game[i][j]=' ';
+                }
+            }
+        }
+        if(s) break;
+    }
+    if(s==0){
+        for(i=1; i<size;i++){
+            for(j=1; j<size ; j++){
+                if(game[i][j]==' '&&(compgame[i][j]== v ||compgame[i][j]==h)){
+                    game[i][j]=compgame[i][j];
+                    notuserwin=1;u=0;
+                    for(i1=1;i1<size;i1++){
+                        for(j1=1;j1<size;j1++){
+                            if(game[i1][j1]==' '&&(compgame[i1][j1]== v ||compgame[i1][j1]==h)){
+                                game[i1][j1]=compgame[i1][j1];
+                                check=checkbox(i1,j1,size,game,compgame,player,totallines, noofmoves, movesplayed, AI);
+                                if(check==1||check==2){
+                                    game[i][j]=' ';
+                                    game[i1][j1]=' ';
+                                    u=1;
+                                    notuserwin=0;
+                                    break;
+                                }
+                                else game[i1][j1]=' ';
+                            }
+                        }
+                        if(u) break;
+                    }
+                    if(notuserwin){s=1; break;}
+                }
+            }
+            if(s) break;
+        }
+    }
+    if(s == 0){
+        for(i = 1; i < size; i++){
+            int bug = 0;
+            for(j = 1; j < size; j++){
+                if(game[i][j] == ' ' && (compgame[i][j] == v || compgame[i][j]== h)){
+                    bug = 1;
+                    break;
+                }
+            }
+            if (bug) break;
+        }
+    }
+    *row = i; *col = j;
 }
 
 int checkbox(int row,int col,int size,char game[size][size],char compgame[size][size],int player,int totallines, int noofmoves, int movesplayed[totallines][7], int ai)
